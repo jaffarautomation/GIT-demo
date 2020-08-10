a1c53304547c4bc6b6f2cb129e1315d68bdaed6f
Jaffar Automation <j0h@example.com>
2020-08-10 14:50:42 +0530

    Git CI Commit

--- src/test/java/testing/GoogleTitle.java
+++ src/test/java/testing/GoogleTitle.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+//Print the title of the Page
 public class GoogleTitle {
 
 	public static void main(String[] args) {
